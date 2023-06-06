c7e535e0bf087f55d73a9ddd0573237863602fa2
SuperNINI831 <130327965+SuperNINI831@users.noreply.github.com>
2023-06-06 19:13:21 +1000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,19 +4,6 @@
 class Calculator:
   
 # add function
-def add(x,y):
-  return x + y
+    def add(x:int , y: int):
+      return x + y
 
-# Substract function
-def substract(x,y):
-  return x - y
-
-# multiply function
-def multiply(x,y):
-  return x * y
-
-# divide function
-def divide(x,y):
-  if y == 0:
-    raise ValueError("divisor cannot be 0")
-  return x / y
